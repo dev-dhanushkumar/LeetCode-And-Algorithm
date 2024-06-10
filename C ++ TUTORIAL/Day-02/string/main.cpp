4dfd7f315f8a14c57a01f6a7611bcdf579bb344d
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-06-10 23:39:14 +0530

    string manipulation

---
+++ C ++ TUTORIAL/Day-02/string/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string abc = "Hi I am Dhanush Kumar";
+
+    cout << abc << "\n";
+
+    cout << abc[0] << "\n";
+    int len = abc.size();
+
+    cout << abc[len - 1] ;
+    return 0;
+}
