a888bb021bf2327b35c1cd2399b5675ccb23170b
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-06-23 23:13:03 +0530

    Hash Map Implementation

---
+++ JAVA_Algorithm/Collections/HashMap/HashMapExample.java
@@ -0,0 +1,17 @@
+package JAVA_Algorithm.Collections.HashMap;
+
+import java.util.*;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+
+        map.put("Dhanush", 45);
+        map.put("Shanak", 76);
+        map.put("Muthu", 55);
+        map.put("sam", 50);
+
+        System.out.println(map);
+    }
+}
