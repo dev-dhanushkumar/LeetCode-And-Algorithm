250699a3997aa3c8bb802c0e994f9ada31b72870
dev-dhanushkumar <gunarakath2711@gmail.com>
2024-06-30 20:55:37 +0530

    Prime Done

---
+++ JAVA_Algorithm/PrimeNum/PrimeNum.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class PrimeNum {
+  static boolean checkPrime(int num) {
+    int cnt = 0;
+
+    for(int  i = 1; i <= Math.sqrt(num); i++){
+      if(num % i == 0) {
+        cnt = cnt + 1;
+
+        if(num / i != i){
+          cnt = cnt + 1;
+        }
+      }
+    }
+
+    if (cnt == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc  = new Scanner(System.in);
+    int num = sc.nextInt();
+
+    boolean isPrime = checkPrime(num);
+    if(isPrime){
+      System.out.println("Given Numbers is PrimeNum");
+    } else {
+      System.out.println("Given Number is not Prime Number");
+    }
+  }
+}
